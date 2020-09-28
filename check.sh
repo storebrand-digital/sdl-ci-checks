497af4d556d5bd4c9e025a1a76daa4e760dbd4e7
Hans Hoegh-Omdal <hahomdal@gmail.com>
2020-09-29 00:47:39 +0200

    add tests to check.sh

--- check.sh
+++ check.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 env 
+
+
+if [ ! -f "security.md" ]; then
+    echo "security.md doesn't exist."
+    exit 1
+fi
+
+# check if security.md is more than 60 days old
+if test `find "security.md" -mtime +60`; then
+  echo "security.md is over 60 days old!"
+  exit 1
+fi
+
+
