1f3e2c381fccf51e477b9d11b66d2469830eebcb
Hans Hoegh-Omdal <Hahomdal@gmail.com>
2020-10-22 14:29:11 +0200

    Use variables

--- check.sh
+++ check.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 env 
+SECF="SECURITY.MD"
+READF="README.MD"
 
 
-if [ ! -f "security.md" ]; then
-    echo "security.md doesn't exist."
+if [ ! -f "${READF}" ]; then
+    echo "${READF} doesn't exist."
+    exit 1
+fi
+
+if [ ! -f "${SECF}" ]; then
+    echo "${SECF} doesn't exist."
     exit 1
 fi
 
 # check if security.md is more than 60 days old
-if test `find "security.md" -mtime +60`; then
-  echo "security.md is over 60 days old!"
+if test `find "${SECF}" -mtime +60`; then
+  echo "${SECF} is over 60 days old!"
   exit 1
 fi
 
