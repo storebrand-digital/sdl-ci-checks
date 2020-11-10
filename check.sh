9fb4ebeb22d85e781570838214b4e2f6bd0a2b7d
Hans Hoegh-Omdal <hahomdal@gmail.com>
2020-11-10 23:02:12 +0100

    Improve error messages when running on github actions

--- check.sh
+++ check.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
-SECF="SECURITY.MD"
-READF="README.MD"
-
 echo -e "Running SDL CI checks.\n"
-
-echo -e "Printing working dir.\n"
-echo -e "PWD=$PWD\n"
 
 
 echo -e "Printing environment variables...\n"
 env 
+SECFILENAME="SECURITY.md"
+READMEFILENAME="README.md"
 
-echo
-
-if [[ -z $(find . -maxdepth 1 -iname "${READF}") ]]; then
-    echo "${READF} doesn't exist."
-    exit 1
-fi
-
-if [[ -z $(find . -maxdepth 1 -iname "${SECF}")  ]]; then
-    echo "${SECF} doesn't exist."
+if [ ! -f "$SECFILENAME" ]; then
+    echo "::error file=${SECFILENAME}::File doesn't exist in repo. This is a requirement in the SDLC: https://wiki.stb.intra/display/DKUF/Phase+2%3A+Design+to+Meet+Security+Requirements"
     exit 1
 fi
 
 # check if security.md is more than 60 days old
-if test `find "${SECF}" -mtime +60`; then
-  echo "${SECF} is over 60 days old!"
+if test `find "$SECFILENAME" -mtime +60`; then
+  echo "::error file=${SECFILENAME}::is over 60 days old!"
   exit 1
 fi
+
+if ! grep -q -i "Security and Privacy Risk Assessment" $SECFILENAME; then
+  echo "::error file=${SECFILENAME}::missing Security and Privacy Risk Assessment section in file!"
+  exit 1
+fi
+
+if ! grep -q -i "Availability Risk Assessment" $SECFILENAME; then
+  echo "::error file=${SECFILENAME}::missing Availability Risk Assessment section in file!"
+  echo "missing Availability Risk Assessment section in $SECFILENAME"
+  exit 1
+fi
+
+if [ ! -f "${READMEFILENAME}" ]; then
+  echo "::error file=${READMEFILENAME}::file doesn't exist!"
+  echo "This is a requirement in the SDLC: https://wiki.stb.intra/display/DKUF/Phase+2%3A+Design+to+Meet+Security+Requirements"
+  exit 1
+fi
+
+
 
 
